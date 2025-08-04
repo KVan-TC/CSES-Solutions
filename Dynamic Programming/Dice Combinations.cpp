69bce5ca653f9484a3856b8942516381cceef0e9
KV-TCode <hasagi2009@gmail.com>
2025-08-05 02:12:31 +0700

    Added Dice Combinations

---
+++ Dynamic Programming/Dice Combinations.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+const int N = 1e6+5, M = 1e9 + 7;
+ 
+int n, dp[N];
+ 
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    cin >> n;
+    dp[0] = 1;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= min(i, 6); j++)
+            dp[i] = (dp[i] + dp[i - j]) % M;
+    cout << dp[n] << "\n";
+
+    return 0;
+}
