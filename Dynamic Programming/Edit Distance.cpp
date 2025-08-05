919b432c5ae7b57e1b83727c2057f0765a72754d
KV-TCode <hasagi2009@gmail.com>
2025-08-05 22:39:05 +0700

    Added Edit Distance

---
+++ Dynamic Programming/Edit Distance.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+const int N = 5e3+5;
+
+string a, b;
+int f[N], g[N];
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    cin >> a >> b;
+    int n = a.size(), m = b.size();
+    a = "#" + a;
+    b = "#" + b;
+
+    for (int i = 0; i <= m; i++)
+        f[i] = i;
+
+    for (int i = 1; i <= n; i++) {
+        g[0] = i;
+        for (int j = 1; j <= m; j++)
+            g[j] = (a[i] == b[j] ? f[j - 1] : min({f[j], g[j - 1], f[j - 1]}) + 1);
+        swap(f, g);
+    }
+
+    cout << f[m] << "\n";
+    return 0;
+}
