df744a6d355d97e1563c54121f9acb29e720710a
KV-TCode <hasagi2009@gmail.com>
2025-08-05 22:09:27 +0700

    Added Book Shop

---
+++ Dynamic Programming/Book Shop.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+const int N = 1e5+5;
+
+int n, W, v[1005], w[1005], f[N];
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    cin >> n >> W;
+
+    for (int i = 0; i < n; i++)
+        cin >> w[i];
+
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+
+    for (int i = 0; i < n; i++)
+        for (int j = W; j >= w[i]; j--)
+            f[j] = max(f[j], f[j - w[i]] + v[i]);
+
+    cout << f[W] << "\n";
+    return 0;
+}
