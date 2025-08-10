6bea7aa6337ed62dcaf32f0910540defe435c77c
KV-TCode <hasagi2009@gmail.com>
2025-08-11 00:53:42 +0700

    Added Trailing Zeros

---
+++ Introductory Problems/Trailing Zeros.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using ll = long long;
+
+int n;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    cin >> n;
+    
+    int ans = 0;
+    for (int p = 5; n >= p; n /= p)
+        ans += n / p;
+    cout << ans << "\n";
+    return 0;
+}
