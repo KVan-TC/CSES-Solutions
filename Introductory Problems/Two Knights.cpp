60235baa750efc0e513662b1b0f0dcc0fdb3e02f
KV-TCode <hasagi2009@gmail.com>
2025-08-11 00:32:32 +0700

    Added Two Knights

---
+++ Introductory Problems/Two Knights.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using ll = long long;
+ 
+ll K;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    cin >> K;
+    for (ll k = 1; k <= K; k++)
+        cout << (1LL + (k - 1) * (k - 2) / 2) * (k - 1) * (k + 4) << "\n";
+    return 0;
+}
+// k^2(k^2+1)/2 - 4(k-1)(k-2) = (1+(k-1)(k-2)/2)(k-1)(k+4)
