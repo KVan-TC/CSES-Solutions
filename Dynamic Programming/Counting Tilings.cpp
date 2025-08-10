bafab6707e32f2d4e5e1a40f88a89745ac2da58a
KV-TCode <hasagi2009@gmail.com>
2025-08-10 20:31:13 +0700

    Added Counting Tilings

---
+++ Dynamic Programming/Counting Tilings.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fi first
+#define se second
+
+using ll = long long;
+using pii = pair<int, int>;
+
+const int N = 10;
+const int M = 1e9 + 7;
+
+int n, m, f[1<<N];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+
+    f[0] = 1;
+    for (; m; m--) {
+        reverse(f, f + (1 << n));
+        for (int i = 0; i < n - 1; i++)
+            for (int mask = 0; mask < (1 << n); mask++)
+                if (!(mask & (3 << i))) 
+                    f[mask] = (f[mask] + f[mask | (3 << i)]) % M;
+    }
+
+    cout << f[0] << "\n";
+    return 0;
+}
