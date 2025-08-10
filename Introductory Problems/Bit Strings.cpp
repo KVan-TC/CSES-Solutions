828811e501bd0a655c1cc37b383279b4721df3bf
KV-TCode <hasagi2009@gmail.com>
2025-08-11 00:48:31 +0700

    Added Bit Strings

---
+++ Introductory Problems/Bit Strings.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using ll = long long;
+
+const int M = 1e9 + 7;
+ 
+int n;
+
+ll pw(ll x, ll n, ll M) {
+    ll a = 1;
+    for (; n; x = x * x % M, n >>= 1)
+        if (n & 1)
+            a = a * x % M;
+    return a;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    cin >> n;
+    cout << pw(2, n, M) << "\n";
+    return 0;
+}
