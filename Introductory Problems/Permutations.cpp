0aec370d692834829d06b7fb83d5f0d4281b39fb
KV-TCode <hasagi2009@gmail.com>
2025-08-11 00:04:03 +0700

    Added Permutations

---
+++ Introductory Problems/Permutations.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using ll = long long;
+ 
+int n;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    cin >> n;
+    if (n == 2 || n == 3) return cout << "NO SOLUTION\n", 0;
+    for (int i = 2; i <= n; i += 2)
+        cout << i << " ";
+    for (int i = 1; i <= n; i += 2)
+        cout << i << " ";
+    return 0;
+}
