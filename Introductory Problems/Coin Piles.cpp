6fab3b28dac5492dd1592f8964ca46b949216e17
KV-TCode <hasagi2009@gmail.com>
2025-08-11 00:57:49 +0700

    Added Coin Piles

---
+++ Introductory Problems/Coin Piles.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using ll = long long;
+
+int T;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    cin >> T;
+    for (int a, b; cin >> a >> b; )
+        cout << ((a + b) % 3 != 0 || a > 2 * b || b > 2 * a ? "NO\n" : "YES\n");
+    return 0;
+}
