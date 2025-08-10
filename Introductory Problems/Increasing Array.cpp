415c7b24898d4ad51b6f812ea3a89b31eb104da5
KV-TCode <hasagi2009@gmail.com>
2025-08-11 00:00:05 +0700

    Added Increasing Array

---
+++ Introductory Problems/Increasing Array.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using ll = long long;
+ 
+int n, mx;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    cin >> n;
+    cin >> mx;
+    ll ans = 0;
+    for (int x; cin >> x; ) {
+        mx = max(mx, x);
+        ans += mx - x;
+    }
+    cout << ans << "\n";
+    return 0;
+}
