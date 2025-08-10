14246270b37d501b4f42b6e951a21756bcff6391
KV-TCode <hasagi2009@gmail.com>
2025-08-10 23:40:55 +0700

    Added Weird Algorithm

---
+++ Introductory Problems/Weird Algorithm.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using ll = long long;
+ 
+ll n;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    cin >> n;
+    while (n > 1) {
+        cout << n << " ";
+        if (n & 1)
+            n = 3 * n + 1;
+        else
+            n >>= 1;
+    }
+    cout << "1\n";
+    return 0;
+}
