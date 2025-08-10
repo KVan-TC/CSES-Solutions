d95411ed09658945f7c9b393b0283b302fed7a81
KV-TCode <hasagi2009@gmail.com>
2025-08-11 00:21:25 +0700

    Added Number Spiral

---
+++ Introductory Problems/Number Spiral.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using ll = long long;
+ 
+int T;
+
+ll sol(ll x, ll y) {
+    ll m = max(x, y), r = (m - 1) * (m - 1);
+    if (m & 1) return r + y + m - x;
+    else return r + x + m - y;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    cin >> T;
+    for (int x, y; cin >> x >> y; ) {
+        cout << sol(x, y) << "\n";
+    }
+    return 0;
+}
