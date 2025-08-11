735135a882940e82fbe8f05d70f42999027cbc8f
KV-TCode <hasagi2009@gmail.com>
2025-08-11 22:46:08 +0700

    Added Digit Queries

---
+++ Introductory Problems/Digit Queries.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int Q;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    
+    cin >> Q;
+    for (int q = 0; q < Q; q++) {
+        ll k; cin >> k;
+        k--;
+
+        int len = 1;
+        ll l = 1, r = 9;
+        while (k >= (r - l  + 1) * len) {
+            k -= (r - l + 1) * len;
+            l *= 10;
+            r = r * 10 + 9;
+            len++;
+        }
+
+        cout << to_string(l + k / len)[k % len] << "\n";
+    }
+
+    return 0;
+}
