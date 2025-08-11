05022207681444ddb29509624ceb04856dd9b93a
KV-TCode <hasagi2009@gmail.com>
2025-08-11 21:23:30 +0700

    Added Raab Game I

---
+++ Introductory Problems/Raab Game I.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using ll = long long;
+
+int T, x[105];
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+ 
+    cin >> T;
+    for (int t = 0; t < T; t++) {
+        int n, a, b; cin >> n >> a >> b;
+        if (a + b > n || ((a == 0 || b == 0) && a + b != 0)) {
+            cout << "NO\n";
+            continue;
+        }
+        cout << "YES\n";
+        for (int i = 1; i <= n; i++)
+            cout << i << " ";
+        cout << "\n";
+        for (int i = 1; i <= b; i++)
+            cout << a + i << " ";
+        for (int i = 1; i <= a; i++)
+            cout << i << " ";
+        for (int i = a + b + 1; i <= n; i++)
+            cout << i << " ";
+        cout << "\n";
+    }
+    return 0;
+}
--- README.md
+++ README.md
@@ -4,8 +4,8 @@ Accepted solutions to the [CSES Problem Set](https://cses.fi/problemset/), writt
 
 | Problem Type          | Number Solved |
 |-----------------------|:-------------:|
-| Introductory Problems |     17/24     |
+| Introductory Problems |     18/24     |
 | Dynamic Programming   |     23/23     |
 | Mathematics           |     10/37     |
 | Geometry              |     3/16      |
-| **Total**             |   **53/400**  |
+| **Total**             |   **54/400**  |
