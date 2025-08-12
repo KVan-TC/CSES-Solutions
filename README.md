a24c760a9804c3afa2bfd383fe6e1db41954602f
KV-TCode <hasagi2009@gmail.com>
2025-08-12 14:19:51 +0700

    Added Maximum Subarray Sum II

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ Accepted solutions to the [CSES Problem Set](https://cses.fi/problemset/), writt
 | Problem Type          | Number Solved |
 |-----------------------|:-------------:|
 | Introductory Problems |     24/24     |
-| Sorting and Searching |     9/35      |
+| Sorting and Searching |     10/35      |
 | Dynamic Programming   |     23/23     |
 | Mathematics           |     10/37     |
 | Geometry              |     3/16      |
-| **Total**             |   **69/400**  |
+| **Total**             |   **70/400**  |
---
+++ Sorting and Searching/Maximum Subarray Sum II.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using ll = long long;
+ 
+const int N = 2e5+5;
+ 
+int n, k, a, b;
+ll S, p[N];
+ 
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+ 
+    cin >> n >> a >> b;
+    for (int i = 1; i <= n; i++) {
+        int x; cin >> x;
+        p[i] = p[i - 1] + x;
+    }
+
+    ll ans = -1e18;
+    deque<int> dq;
+    for (int r = a; r <= n; r++) {
+        int l1 = max(0, r - b), l2 = r - a;
+
+        while (!dq.empty() && dq.front() < l1) dq.pop_front();
+
+        if (l2 >= 0) {
+            while (!dq.empty() && p[dq.back()] >= p[l2]) dq.pop_back();
+            dq.push_back(l2);
+        }
+
+        if (!dq.empty()) ans = max(ans, p[r] - p[dq.front()]);
+    }
+
+    cout << ans << "\n";
+    return 0;
+}
