f79dd0e75d9e23985a1e107fe82b8c4a02b467fb
KV-TCode <hasagi2009@gmail.com>
2025-08-12 12:18:20 +0700

    Added Sum of Two Values

---
+++ Sorting and Searching/Sum of Two Values.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+
+const int N = 2e5+5;
+
+int n, X;
+
+pii a[N];
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    cin >> n >> X;
+    for (int i = 0; i < n; i++) {
+        int x; cin >> x;
+        a[i] = {x, i + 1};
+    }
+    sort(a, a + n);
+
+    int l = 0, r = n - 1;
+    while (l < r) {
+        int s = a[l].first + a[r].first;
+        if (s == X) {
+            cout << a[l].second << " " << a[r].second << "\n";
+            return 0;
+        } else if (s > X) r--;
+        else l++;
+    }
+    cout << "IMPOSSIBLE\n";
+    return 0;
+}
