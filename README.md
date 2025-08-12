7ad74eb8f713513ce060298ee61da725b1b9f304
KV-TCode <hasagi2009@gmail.com>
2025-08-13 02:33:14 +0700

    Added Finding Periods

--- README.md
+++ README.md
@@ -9,6 +9,6 @@ Accepted solutions to the [CSES Problem Set](https://cses.fi/problemset/), writt
 | Dynamic Programming   |     23/23     |
 | Graph Algorithms      |     3/36      |
 | Mathematics           |     10/37     |
-| String Algorithms     |     3/21      |
+| String Algorithms     |     4/21      |
 | Geometry              |     3/16      |
-| **Total**             |   **76/400**  |
+| **Total**             |   **77/400**  |
---
+++ String Algorithms/Finding Periods.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using ll = long long;
+ 
+const int N = 1e6+5;
+ 
+string S;
+int LPS[N];
+ 
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+ 
+    cin >> S;
+    int n = S.size();
+
+    for (int i = 1; i < n; i++) {
+        int j = LPS[i - 1];
+        while (j > 0 && S[i] != S[j]) j = LPS[j - 1];
+        if (S[i] == S[j]) j++;
+        LPS[i] = j;
+    }
+    
+    vector<int> ans;
+    for (int k = LPS[n - 1]; k > 0; k = LPS[k - 1])
+        ans.push_back(n - k);
+
+    ans.push_back(n);
+
+    sort(ans.begin(), ans.end());
+    ans.erase(unique(ans.begin(), ans.end()), ans.end());
+    for (int x : ans)
+        cout << x << " ";
+    cout << "\n";
+    return 0;
+}
