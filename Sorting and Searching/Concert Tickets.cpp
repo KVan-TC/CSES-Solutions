dbba8cfee1b5457a7a44a9493bf768fee1d298de
KV-TCode <hasagi2009@gmail.com>
2025-08-14 00:22:44 +0700

    Added Concert Tickets

---
+++ Sorting and Searching/Concert Tickets.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using ll = long long;
+
+const int N = 2e5+5;
+
+int n, m, a[N], ds[N];
+
+int find(int u) {
+    if (u < 0) return -1;
+    return (ds[u] == u ? u : ds[u] = find(ds[u]));
+}
+
+void remove(int u) {
+    ds[u] = find(u - 1);
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+ 
+    cin >> n >> m;
+
+    iota(ds, ds + n, 0);
+
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    
+    sort(a, a + n);
+
+    for (int i = 0; i < m; i++) {
+        int t; cin >> t;
+        int pos = upper_bound(a, a + n, t) - a - 1;
+        if (pos < 0) {
+            cout << "-1\n";
+            continue;
+        }
+        int p = find(pos);
+        if (p < 0)
+            cout << "-1\n";
+        else {
+            cout << a[p] << "\n";
+            remove(p);
+        }
+    }
+    return 0;
+}
