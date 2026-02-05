edf663c0f6882e4aee76e3105d233c78aa8c929e
KVan-TC <hasagi2009@gmail.com>
2026-02-05 11:31:38 +0700

    Fixed spacing

--- Range Queries/Range Xor Queries.cpp
+++ Range Queries/Range Xor Queries.cpp
@@ -1,24 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-const int N = 2e5+5;;
  
-int n, Q, p[N];
+typedef long long ll;
+ 
+const int N = 2e5+5;
+ 
+int n, Q;
+ll f[N];
  
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
  
     cin >> n >> Q;
- 
     for (int i = 1; i <= n; i++) {
         int x; cin >> x;
-        p[i] = p[i - 1] ^ x;
+        f[i] = f[i - 1] ^ x;
     }
  
-    for (int q = 0; q < Q; q++) {
+    for (int i = 0; i < Q; i++) {
         int l, r; cin >> l >> r;
- 
-        cout << (p[r] ^ p[l - 1]) << "\n";
+        cout << (f[r] ^ f[l - 1]) << "\n";
     }
     return 0;
 }
