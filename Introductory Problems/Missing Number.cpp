d246913032b6ca1224bc69b5d2a3f6b809946c2e
KVan-TC <hasagi2009@gmail.com>
2026-02-19 00:15:42 +0700

    Fixed Missing Number

--- Introductory Problems/Missing Number.cpp
+++ Introductory Problems/Missing Number.cpp
@@ -1,17 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
  
+typedef long long ll;
+ 
 int n;
-
+ 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
  
     cin >> n;
-    int num = 0;
+    ll num = 0;
     for (int i = 1; i <= n; i++)
         num ^= i;
-    for (int x; cin >> x; )
+    for (int i = 1; i < n; i++) {
+        int x; cin >> x;
         num ^= x;
+    }
+ 
     cout << num << "\n";
     return 0;
 }
